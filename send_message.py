a142577706f46cf28a0163550e52be46d1314afd
RamoAndMe <83104627+RamiAndMe@users.noreply.github.com>
2021-07-31 06:50:02 +0400

    teesat

---
+++ send_message.py
@@ -0,0 +1,20 @@
+# Allow Python to Send HTTP request
+import requests
+
+# Pretty Printer
+from pprint import pprint
+
+# Setting up some variables...
+BOT_TOKEN = '<1939656470:AAGPRsDVZMqvKBlhul_Bd_1Xxgen-K8DHzg>'
+BOT_URL = 'https://api.telegram.org/cesusbot' + BOT_TOKEN + '/'
+
+
+# Send Message (/sendMessage)
+data = {
+    'chat_id': <Insert Chat ID>,
+    'text': <Write whatever you want>
+}
+response = __________________
+
+# Print Response
+pprint(response.json())
